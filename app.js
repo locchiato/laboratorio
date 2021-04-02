54c5f1e7ae5b305715996b387cca5354782dee5b
Leandro Occhiato <leandro.occhiato@gmail.com>
2021-04-02 20:10:04 -0300

    [MOD] Optimización.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // (1)
 function invertirArray(arrayDeDatos) {
   /*
+  // no mantiene inmutable al objeto original
+  // resolucion con 1 return
   return [
     deUnoHastaCuatro.pop(),
     deUnoHastaCuatro.pop(),
@@ -8,23 +10,27 @@ function invertirArray(arrayDeDatos) {
     deUnoHastaCuatro.pop()
   ];
 */
-
   const nuevoArray = [];
 
+  for (let i = 0; i < arrayDeDatos.length; i++) {
+    const dato = arrayDeDatos.pop();
+    arrayDeDatos.unshift(dato);
+    nuevoArray.push(dato);
+  }
+  /*
+  // no utiliza .pop()
   nuevoArray.push(arrayDeDatos[3]);
   nuevoArray.push(arrayDeDatos[2]);
   nuevoArray.push(arrayDeDatos[1]);
   nuevoArray.push(arrayDeDatos[0]);
-  /*
-  for(let i=0; i<4; i++){
-  nuevoArray.push(arrayDeDatos.pop());
-  }
-
+  
+  // no mantiene inmutable al objeto original
   nuevoArray.push(deUnoHastaCuatro.pop()); // nA = [4]
   nuevoArray.push(deUnoHastaCuatro.pop()); // nA = [4, 3]
   nuevoArray.push(deUnoHastaCuatro.pop()); // nA = [4, 3, 2]
   nuevoArray.push(deUnoHastaCuatro.pop()); // nA = [4, 3, 2, 1]
 */
+
   return nuevoArray;
 }
 
@@ -40,20 +46,20 @@ function esPar(num) {
 
 function soloPares(numeros) {
   const pares = [];
-  for (let i = 0; i < numeros.length; i++) {
-    const num = numeros[i];
-    esPar(num) ? pares.push(num) : 0;
-  }
+
+  for (let i = 0; i < numeros.length; i++)
+    esPar(numeros[i]) ? pares.push(numeros[i]) : 0;
+
   return pares;
 }
 
 function soloImpares(numeros) {
-  const pares = [];
-  for (let i = 0; i < numeros.length; i++) {
-    const num = numeros[i];
-    esPar(num) ? 0 : pares.push(num);
-  }
-  return pares;
+  const impares = [];
+
+  for (let i = 0; i < numeros.length; i++)
+    esPar(numeros[i]) ? 0 : impares.push(numeros[i]);
+
+  return impares;
 }
 
 const nums = [1, 2, 3, 4];
@@ -64,13 +70,12 @@ console.log(soloImpares(nums));
 
 // (3)
 function tipoArray(tipoDeDato, arrayDeDatos) {
-  for (let i = 0; i < arrayDeDatos.length; i++) {
-    const dato = arrayDeDatos[i];
-    if(tipoDeDato !== typeof dato)
-      return false;
-  }
+  for (let i = 0; i < arrayDeDatos.length; i++)
+    if (tipoDeDato !== typeof arrayDeDatos[i]) return false;
+
   return true;
-/*
+  /*
+  // resolucion con 1 return
   return (
     tipoDeDato === typeof arrayDeDatos[0] &&
     tipoDeDato === typeof arrayDeDatos[1] &&
@@ -91,13 +96,10 @@ function sumar(numeros) {
   if (tipoArray("number", numeros) === false) {
     return "Error: array corrupto";
   }
-  //  return numeros[0] + numeros[1] + numeros[2] + numeros[3];
 
+  //  return numeros[0] + numeros[1] + numeros[2] + numeros[3];
   let suma = 0;
-  for (let i = 0; i < numeros.length; i++) {
-    const num = numeros[i];
-    suma += num;
-  }
+  for (let i = 0; i < numeros.length; i++) suma += numeros[i];
 
   return suma;
 }
@@ -110,6 +112,8 @@ console.log(sumar(["1", "2", "hola", "4"])); // Error: array corrupto
 
 // (5)
 function detalles(datos) {
+  console.log("------------------------------");
+  console.log();
   console.log("Datos ingresados: " + datos);
   console.log("Orden inverso: " + invertirArray(datos));
   console.log();
@@ -123,28 +127,26 @@ function detalles(datos) {
   console.log("Suma de datos pares: " + sumar(soloPares(datos)));
   console.log();
   console.log("Datos ingresados: " + datos);
-  console.log("Los pares del orden inverso: " + soloPares(invertirArray(datos)));
+  console.log(
+    "Los pares del orden inverso: " + soloPares(invertirArray(datos))
+  );
   console.log();
   console.log("Datos ingresados: " + datos);
-  console.log("Suma de los impares del orden inverso: " + sumar(soloImpares(invertirArray(datos))));
+  console.log(
+    "Suma de los impares del orden inverso: " +
+      sumar(soloImpares(invertirArray(datos)))
+  );
   console.log();
 }
 console.log();
+console.log("------------------------------");
 console.log("- 5 --------------------------");
+console.log("------------------------------");
 console.log("A continuacion se mostraran en orden los siguientes topicos:");
 console.log(
-  "Datos ingresados - Orden Inverso - Suma de valores - Datos pares.");
-console.log();
+  "Datos ingresados - Orden Inverso - Suma de valores - Datos pares."
+);
 detalles([1, 2, 3, 4]);
-console.log();
-console.log("------------------------------");
-console.log();
 detalles([1, 2, "hola", 4]);
-console.log();
-console.log("------------------------------");
-console.log();
 detalles(["1", "2", "hola", "4"]);
-console.log();
-console.log("------------------------------");
-console.log();
-detalles(['1', '2', '3', '4']);
+detalles(["1", "2", "3", "4"]);
