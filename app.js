d5a45088648bfdf99ef82c973273d593a60a119a
Leandro Occhiato <leandro.occhiato@gmail.com>
2021-04-02 15:59:49 -0300

    [MOD] agrego ejercicio 2, 3 y 4.

--- app.js
+++ app.js
@@ -16,5 +16,45 @@ const invertir = function (deUnoHastaCuatro) {
   return deCuatroHastaUno;
 };
 
-const arr = [1, 2, 3, 4];
-console.log(invertir(arr));
+const numeros = [1, 2, 3, 4];
+console.log(invertir(numeros));
+
+// (2)
+function soloPares(numeros) {
+  const pares = [];
+  for (let i = 0; i < numeros.length; i++) {
+    const obj = numeros[i];
+    obj % 2 === 0 ? pares.push(obj) : "";
+  }
+  return pares;
+}
+
+const nums = [1, 2, 3, 4];
+console.log(soloPares(nums));
+
+// (3)
+function tipoArray(tipoDeDato, arrayConDatos) {
+  return (
+    tipoDeDato == typeof arrayConDatos[0] &&
+    tipoDeDato == typeof arrayConDatos[1] &&
+    tipoDeDato == typeof arrayConDatos[2] &&
+    tipoDeDato == typeof arrayConDatos[3]
+  );
+}
+console.log("------------------------------");
+console.log(tipoArray("number", [1, 2, 3, 4])); // true
+console.log(tipoArray("number", [1, 2, "hola", 4])); //false
+console.log(tipoArray("string", ["1", "2", "hola", "4"])); //true
+
+// (4)
+function suma(numeros) {
+  if (tipoArray("number", numeros) === false)
+  return "Error: array corrupto";
+
+  return numeros[0] + numeros[1] + numeros[2] + numeros[3];
+}
+
+console.log("------------------------------");
+console.log(suma([1, 2, 3, 4])); // true
+console.log(suma([1, 2, "hola", 4])); //false
+console.log(suma(["1", "2", "hola", "4"])); //true
